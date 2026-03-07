928a5350576d8297bd85132759b6ba05112fa04f
Sabba Hossain <sabbahossain@gmail.com>
2026-03-07 12:31:21 +0600

    ad

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="border-t border-border bg-card pb-6 pt-16">
+        <footer className="border-t border-border bg-card  pt-16">
             <div className="mx-auto max-w-6xl px-4">
                 <div className="grid gap-12 lg:grid-cols-4">
                     <div className="lg:col-span-1">
