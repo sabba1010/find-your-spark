8e8754b9586c872450ed1849357c0a27a0106102
Sabba Hossain <sabbahossain@gmail.com>
2026-03-14 10:23:44 +0600

    add

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     },
     proxy: {
       "/api": {
-        target: "https://amour-et-sincerite.com/api",
+        target: "http://localhost:5000",
         changeOrigin: true,
       },
     },
